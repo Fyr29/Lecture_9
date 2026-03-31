1cee98b85da535bdd39e3f65c593452064a86add
Фыр <danil.erlih@gmail.com>
2026-03-31 13:55:36 +0300

    Home work 1

---
+++ main.py
@@ -0,0 +1,16 @@
+result = {}
+
+def popular_words (text, words):
+
+    for word in words:
+        result[word] = text.lower().split().count(word)
+
+    return result
+
+
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''',
+                     ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 },\
+                            'Test1'
+
+
+print(f'{result}\n\nOK')
